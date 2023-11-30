1f85fad26f6926950e47851407b494becf305823
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-11-30 10:50:20 +0700

    Clamping the player positions

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,2 +1,20 @@
 extends CharacterBody2D
 
+@export var speed := 300
+
+func _physics_process(delta: float) -> void:
+	velocity = Vector2.ZERO
+	
+	if Input.is_action_pressed("move_right"):
+		velocity.x = speed
+	if Input.is_action_pressed("move_left"):
+		velocity.x = -speed
+	if Input.is_action_pressed("move_up"):
+		velocity.y = -speed
+	if Input.is_action_pressed("move_down"):
+		velocity.y = speed
+		
+	move_and_slide()
+	
+	var screen_size := get_viewport_rect().size
+	global_position = global_position.clamp(Vector2.ZERO, screen_size)
