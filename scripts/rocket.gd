8b95e56ae071e8597bfec8a237507442dfc32b76
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-11-30 19:52:51 +0700

    Deleting unwanted rockets

--- scripts/rocket.gd
+++ scripts/rocket.gd
@@ -4,5 +4,13 @@ class_name Rocket
 
 @export var speed := 300
 
+@onready var visible_notifier: VisibleOnScreenNotifier2D = $VisibleNotifier
+
+func  _ready() -> void:
+	visible_notifier.connect("screen_exited", _on_screen_exited)
+
 func _physics_process(delta: float) -> void:
 	global_position.x += speed * delta
+
+func _on_screen_exited() -> void:
+	queue_free()
