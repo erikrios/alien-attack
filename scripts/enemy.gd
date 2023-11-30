483ec3f84ef7e542c517403b818cc1ec29685434
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-11-30 20:54:35 +0700

    Spawning the enemy

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+class_name Enemy
+
 @export var speed := 300
 
 func _physics_process(delta: float) -> void:
--- scripts/enemy_spawner.gd
+++ scripts/enemy_spawner.gd
@@ -1,5 +1,16 @@
 extends Node2D
 
+@onready var spawn_positions: Node2D = $SpawnPositions
+
+var enemy_scene: PackedScene = preload("res://scenes/enemy.tscn")
 
 func _on_timer_timeout() -> void:
-	print("Timeout")
+	spawn_enemy()
+
+func spawn_enemy() -> void:
+	var spawn_position_children := spawn_positions.get_children()
+	var random_spawn_position: Marker2D = spawn_position_children.pick_random()
+	
+	var enemy_instance := enemy_scene.instantiate()
+	enemy_instance.global_position = random_spawn_position.global_position
+	add_child(enemy_instance)
