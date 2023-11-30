0a12ceb2cedf210773de87500f97468de6a921dc
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-11-30 19:40:44 +0700

    Creating references to nodes

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,6 +6,8 @@ class_name Player
 
 var rocket_scene: PackedScene = preload("res://scenes/rocket.tscn")
 
+@onready var rocket_container: Node = $RocketContainer
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
@@ -29,5 +31,6 @@ func _physics_process(delta: float) -> void:
 
 func shoot() -> void:
 	var rocket_instance: Rocket = rocket_scene.instantiate()
-	add_child(rocket_instance)
+	rocket_container.add_child(rocket_instance)
+	rocket_instance.global_position = global_position
 	rocket_instance.global_position.x += 80
