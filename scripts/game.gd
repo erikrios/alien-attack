79004cc27046e5b1f4ca60f4a08c8feefc53d94f
Erik Rio Setiawan <erikriosetiawan15@gmail.com>
2023-12-01 10:32:45 +0700

    Adding lives UI

--- scripts/game.gd
+++ scripts/game.gd
@@ -8,17 +8,17 @@ var score := 0
 
 func _ready() -> void:
 	hud.set_score_label(score)
+	hud.set_lives(lives)
 
 func _on_death_zone_area_entered(area: Area2D) -> void:
 	(area as Enemy).die()
 
 func _on_player_took_damage() -> void:
 	lives -= 1
+	hud.set_lives(lives)
 	if lives == 0:
 		player.die()
 		print("Game over")
-	else:
-		print("Lives: " + str(lives))
 
 func _on_enemy_spawner_enemy_spawned(enemy_instance: Enemy) -> void:
 	enemy_instance.connect("died", _on_enemy_died)
--- scripts/hud.gd
+++ scripts/hud.gd
@@ -2,7 +2,11 @@ extends Control
 
 class_name HUD
 
-@onready var score: Label = $Score
+@onready var score: Label = %Score
+@onready var lives_left: Label = %LivesLeft
 
 func set_score_label(new_score: int): 
 	score.text = "SCORE: %s" % str(new_score)
+	
+func set_lives(amount: int):
+	lives_left.text = str(amount)
